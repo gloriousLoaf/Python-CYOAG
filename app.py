5e4320f2eb17bc4c9d304102294ff5b7db2dd476
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-12-21 16:52:53 -0800

    Improved Act I of the game. Wrote the beginning or Act II.

--- app.py
+++ app.py
@@ -39,19 +39,64 @@ else:
         f'Hmph, I hope you\'ve had your vitamins, {name}, this will be tough!')
 
 print(f'Alright {name} who claims to be {age} years old. Your journey awaits!')
-start = input('Shall we depart? Y / N ').lower()
 
-if start == 'n' or 'no':
-    print(f'I did not think you were ready either, {name}. Farewell.')
-else:
+# Shall we begin?
+health = 10
+start = input('Shall we depart? Y / N ').lower()
+if start == 'y' or start == 'yes':
     print(
         f'Glorious, another victim for the snake! \nI mean, victory awaits you, '
-        f'{name}! Or whatever people say...')
+        f'{name}! Or whatever people say... \nYou are beginning your journey with'
+        f' 10 health points.')
 
+    # ACT I: The River or the Swamp
+    north_east = input('Will you go north or east? (north/east) ').lower()
+    if north_east == 'east':
+        print(
+            'You travel east until the road meets the marsh. \nWolves descend'
+            ' upon you, and you perish.')
+    elif north_east == 'north':
+        print('Excellent, yes let\'s head straight towards the danger.')
+        print(20 * '-')
+        river = input(
+            'After traveling many miles north, you encounter the great'
+            ' Green River. \nWill you wait for a ferryman or attempt to cross?'
+            ' (wait/cross) ')
+        if river == 'wait':
+            health += 3
+            print(f'The ferryman arrives hours later, with rations. After'
+                  f' eating and crossing the river, \nyour health is now {health}.')
+        elif river == 'cross':
+            health -= 3
+            print(f'You swam across, but rough currents dashed you against the'
+                  f' rocks. Your health is now {health}')
+        else:
+            print(
+                'I\'m calling this off. You lack basic communication skills.'
+                ' If you cannot wield a keyboard, you surely cannot wield a sword.')
 
-# ACT I: The river or the swamp.
-# north_east = input('Will you go North or East? (north/east)?')
-# if north_east == 'east':
-#     print('You travel east until the road meets the marsh. \nWolves descend upon you,'
-#           f' and you perish.')
-# else:
+        # ACT II: The Village or the Market
+        print(20 * '-')
+        resting_place = input(
+            'After a long day of travelling you encounter a crossing path. \nA crude'
+            ' sign indicates that going northeast leads to a market,\nwhile'
+            ' going west will take you to a small village. \nWhich way'
+            ' will you go to rest before continuing? (market/village) ')
+        if resting_place == 'market':
+            health -= 3
+            print(
+                f'After haggling with merchants, you are offered a mat to sleep on,'
+                f' \nand a small sword which you can use to fight Jörmgandr. \n The'
+                f' mat is uncomfortable and hurts your back. You health is now {health}.')
+        elif resting_place == 'village':
+            health += 3
+            print(
+                f'The villagers are poor but kind. You are offered a warm bed,'
+                f'\nand meet a skilled ironsmith. He gives you a great sword to fight'
+                f' the wicked Jörmgandr. \nRested and armed, you health is now {health}.')
+    else:
+        print(
+            'I\'m calling this off. You lack basic communication skills.'
+            ' If you cannot wield a keyboard, you surely cannot wield a sword.')
+else:
+    print(f'I did not think you were ready either. Farewell, {name}.')
